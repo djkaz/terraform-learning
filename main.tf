b57608dbd64b57ef7f856a1b122a6191c15bdaba
Tiron Alexandru-Stefan <stefiaxe@yahoo.com>
2025-08-05 23:16:00 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,25 +1,33 @@
-# main.tf
-
-# 1. Configure the AWS Provider
-#    This tells Terraform to use a specific region and
-#    provides dummy credentials for LocalStack.
-provider "aws" {
-  region                      = "us-east-1"
-  access_key                  = "test"
-  secret_key                  = "test"
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = true
-  s3_use_path_style           = true # Recommended for LocalStack S3
-
-  # Configure the endpoint for LocalStack
-  endpoints {
-    s3 = "http://localhost:4566"
-  }
+terraform {
+    required_version = ">= 0.12"
+    backend "local" {
+      
+    }
 }
 
-# 2. Define the resource you want to create
-#    This is the declarative part of Terraform.
-resource "aws_s3_bucket" "my_first_local_bucket" {
-  bucket = "my-awesome-local-bucket"
+provider "aws" {
+    region = "us-east-1"
+    access_key = "test"
+    secret_key = "test"
+    skip_credentials_validation = true
+    endpoints {
+        #ec2 = "http://localhost:4566"
+        iam = "http://localhost:4567"
+    }
+}
+
+resource "aws_iam_role" "test_role" {
+    name = "test_role"
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Action = "sts:AssumeRole"
+                Effect = "Allow"
+                Principal = {
+                    Service = "ec2.amazonaws.com"
+                }
+            }
+        ]
+    })
 }
