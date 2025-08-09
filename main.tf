1ac75613b714150f7de2398c886e5d69d174b493
Tiron Alexandru-Stefan <stefiaxe@yahoo.com>
2025-08-09 23:13:51 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,25 +1,29 @@
-# main.tf
-
-# 1. Configure the AWS Provider
-#    This tells Terraform to use a specific region and
-#    provides dummy credentials for LocalStack.
-provider "aws" {
-  region                      = "us-east-1"
-  access_key                  = "test"
-  secret_key                  = "test"
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = true
-  s3_use_path_style           = true # Recommended for LocalStack S3
-
-  # Configure the endpoint for LocalStack
-  endpoints {
-    s3 = "http://localhost:4566"
-  }
+#creating an ec2 instance simple
+terraform {
+    required_providers {
+        aws = {
+        source  = "hashicorp/aws"
+        version = "~> 5.0"
+        }
+    }
 }
 
-# 2. Define the resource you want to create
-#    This is the declarative part of Terraform.
-resource "aws_s3_bucket" "my_first_local_bucket" {
-  bucket = "my-awesome-local-bucket"
+
+provider "aws" {
+    region = "us-east-1"
+    access_key =  "test"
+    secret_key = "test"
+    skip_credentials_validation = true
+    skip_metadata_api_check = true
+    endpoints {
+        ec2 = "http://localhost:4566"
+    }
+}
+
+resource "aws_instance" "smechera" {
+    ami           = "ami-12345678"
+    instance_type = "t2.micro"
+    tags = {
+        Name = "smechera"
+    }  
 }
