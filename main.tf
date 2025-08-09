6d856ba2443ba19a3bea3bb69f9d9c69312dcd1e
Tiron Alexandru-Stefan <stefiaxe@yahoo.com>
2025-08-10 00:17:13 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,25 +1,39 @@
-# main.tf
+terraform {
+    required_providers {
+        aws = {
+        source  = "hashicorp/aws"
+        version = "~> 5.0"
+        }
+    }
+}
 
-# 1. Configure the AWS Provider
-#    This tells Terraform to use a specific region and
-#    provides dummy credentials for LocalStack.
 provider "aws" {
-  region                      = "us-east-1"
-  access_key                  = "test"
-  secret_key                  = "test"
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = true
-  s3_use_path_style           = true # Recommended for LocalStack S3
-
-  # Configure the endpoint for LocalStack
+  access_key               = "mock-access-key"
+  secret_key               = "mock-secret-key"
+  region                   = "us-east-1"
+  skip_requesting_account_id = true
   endpoints {
-    s3 = "http://localhost:4566"
+    iam = "http://localhost:4566"
   }
 }
 
-# 2. Define the resource you want to create
-#    This is the declarative part of Terraform.
-resource "aws_s3_bucket" "my_first_local_bucket" {
-  bucket = "my-awesome-local-bucket"
+resource "aws_iam_role" "test_role" {
+    name = "test_role"
+
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Effect = "Allow"
+                Principal = {
+                    Service = "ec2.amazonaws.com"
+                }
+                Action = "sts:AssumeRole"
+            }
+        ]
+    })
+}
+
+output "role_arn" {
+  value = aws_iam_role.test_role.arn
 }
