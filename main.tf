316334a563d94ca11a180bfb20716cc96fb7b172
Tiron Alexandru-Stefan <stefiaxe@yahoo.com>
2025-08-10 17:10:34 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,25 +1,74 @@
-# main.tf
+terraform {
+    required_providers {
+        aws = {
+        source  = "hashicorp/aws"
+        version = "~> 5.0"
+        }
+    }
+}
 
-# 1. Configure the AWS Provider
-#    This tells Terraform to use a specific region and
-#    provides dummy credentials for LocalStack.
 provider "aws" {
-  region                      = "us-east-1"
-  access_key                  = "test"
-  secret_key                  = "test"
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = true
-  s3_use_path_style           = true # Recommended for LocalStack S3
-
-  # Configure the endpoint for LocalStack
+  access_key               = "test"
+  secret_key               = "test"
+  region                   = "us-east-1"
+  skip_requesting_account_id = true
   endpoints {
-    s3 = "http://localhost:4566"
+    iam = "http://localhost:4566"
   }
 }
 
-# 2. Define the resource you want to create
-#    This is the declarative part of Terraform.
-resource "aws_s3_bucket" "my_first_local_bucket" {
-  bucket = "my-awesome-local-bucket"
+
+# Create an IAM role
+resource "aws_iam_role" "read_role" {
+  name = "read_kms_s3_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# Create an IAM policy for KMS and S3 read access
+resource "aws_iam_policy" "read_policy" {
+  name        = "read_kms_s3_policy"
+    policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "kms:Decrypt",
+          "s3:GetObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          "arn:aws:s3:::my-awesome-local-bucket",
+          "arn:aws:s3:::my-awesome-local-bucket/*"
+        ]
+    },
+        {
+            Effect = "Allow"
+            Action = [
+            "kms:DescribeKey",
+            "kms:ListAliases"
+            ]
+            Resource = "*"
+        }
+        ]
+    })
+}
+
+# Attach the policy to the role
+resource "aws_iam_role_policy_attachment" "read_policy_attachment" {
+    role = aws_iam_role.read_role.name
+    policy_arn = aws_iam_policy.read_policy.arn
 }
